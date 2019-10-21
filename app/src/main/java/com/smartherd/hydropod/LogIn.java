b6a4b46f0d635f2c00c3703f4472a9435ba9a561
kanvasuchiha <kanav99157@gmail.com>
2019-10-22 05:12:12 +0530

    Dashbard work started

--- app/src/main/java/com/smartherd/hydropod/LogIn.java
+++
@@ -1,6 +0,0 @@
-package com.smartherd.hydropod;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class LogIn extends AppCompatActivity {
-}
--- app/src/main/java/com/smartherd/hydropod/MainActivity.java
+++ app/src/main/java/com/smartherd/hydropod/MainActivity.java
@@ -18,10 +18,12 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,13 +50,34 @@ import com.parse.SignUpCallback;
 public class MainActivity extends AppCompatActivity {
 
 
+
+
+    ProgressBar progressBar;
+    TextView textView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_screen);
 
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        progressBar=findViewById(R.id.progressBar);
+        textView=findViewById(R.id.textLoad);
 
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
+
+    }
+
+    public void progressAnimation()
+    {
+        ProgressBarAnimation amin= new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
+        amin.setDuration(8000);
+        progressBar.setAnimation(amin);
     }
 
 }
